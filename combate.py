bcb3d56bd17f56cc8b4f14a3b6b4ba70d16cdc71
yeon333 <tjdus443@naver.com>
2021-01-21 19:31:45 +0900

    crawling athle name and download img

---
+++ combate.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*- 
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import urllib.request
+#beautiful soup
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+driver1 = webdriver.Chrome()
+driver1.get('https://kr.ufc.com/athletes/all')
+time.sleep(3)
+#while True: #scroll down & click theload button
+#	try:
+#		####scroll
+#		SCROLL_PAUSE_TIME = 2
+
+#		# Get scroll height
+#		last_height = driver1.execute_script("return document.body.scrollHeight")        
+#		b=1
+#		while True:
+			
+#			# Scroll down to bottom                                                    
+#			driver1.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+#			# Wait to load page
+#			time.sleep(SCROLL_PAUSE_TIME)                                               
+#			driver1.execute_script("window.scrollTo(0, document.body.scrollHeight-50);") 
+#			time.sleep(SCROLL_PAUSE_TIME)
+
+#			# Calculate new scroll height and compare with last scroll height           
+#			new_height = driver1.execute_script("return document.body.scrollHeight")
+
+#			if new_height == last_height:                                              
+#				break
+
+#			last_height = new_height
+#		####scroll end 
+#		more_athle=driver1.find_element_by_xpath('/html/body/div[1]/div/main/div[1]/div/div/div/div/div/div/div/div/div[1]/div/div/ul/li/a')
+#		more_athle.click()
+		
+	
+#	except:
+#		print("nope while")
+#		break
+	
+html=driver1.page_source 
+#soup=BeautifulSoup(html,'lxml')  
+soup = BeautifulSoup(html, 'html.parser') 
+a = 1
+while True:	
+	for anchor in soup.select("div.c-listing-athlete-flipcard__text__back span.c-listing-athlete__name"):
+		print(str(a) + ": " + anchor.get_text())
+		a+=1
+		driver = webdriver.Chrome()
+	
+		driver.get("https://www.google.co.kr/imghp?hl=ko&tab=wi&ogbl")
+		elem=driver.find_element_by_name("q")
+		time.sleep(5)
+		elem.send_keys(anchor.get_text())
+		time.sleep(5)
+		#driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[2]/form/div[2]/div[1]/div[1]/button").click()
+		#elem.send_keys(Keys.RETURN)
+		#driver.find_element_by_css_selector("Tg7LZd").click()
+		#try:
+		#	elem.send_keys(Keys.RETURN)
+		#except:
+		#	pass
+		
+		time.sleep(3)
+
+		sibal=driver.find_element_by_css_selector(".JSAgYe").get_attribute("value")
+		images=driver.find_elements_by_css_selector(".rg_i.Q4LuWd")
+		count=1
+		
+		for image in images: 
+			try:
+				image.click()
+				time.sleep(2)
+				imgUrl=driver.find_element_by_xpath('/html/body/div[2]/c-wiz/div[3]/div[2]/div[3]/div/div/div[3]/div[2]/c-wiz/div[1]/div[1]/div/div[2]/a/img').get_attribute("src")
+						
+				urllib.request.urlretrieve(imgUrl,sibal+str(count)+'.jpg')
+				count+=1
+			except:
+				pass    
+		driver.close()
+
