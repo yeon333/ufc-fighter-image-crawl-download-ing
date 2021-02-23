d475873201da360af882401e1ecb6e3743d3264d
yeon333 <tjdus443@naver.com>
2021-02-23 16:40:51 +0900

    combate.py-> women.py  and fix download bug

---
+++ women.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*- 
+from selenium import webdriver  #pip install selenium
+from selenium.webdriver.common.keys import Keys
+import time
+import urllib.request
+import requests  # pip install requests
+import shutil  
+from urllib.error import URLError, HTTPError
+#beautiful soup
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+#import subprocess
+
+driver = webdriver.Chrome()
+driver.get('https://kr.ufc.com/athletes/all')
+time.sleep(2)
+fe_athle=driver.find_element_by_xpath('/html/body/div[1]/div/main/div[1]/div/div/div/div/div/div/div/div/div[2]/div/div/form/div/div[1]/div/div[2]')
+fe_athle.click()
+time.sleep(3)
+ 
+SCROLL_PAUSE_TIME = 3.5
+# Get scroll height
+last_height = driver.execute_script("return document.body.scrollHeight")
+while True:
+	# Scroll down to bottom
+	driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+	# Wait to load page
+	time.sleep(SCROLL_PAUSE_TIME)
+	# Calculate new scroll height and compare with last scroll height
+	new_height = driver.execute_script("return document.body.scrollHeight")
+	if new_height == last_height:
+		try:
+			more_athle=driver.find_element_by_xpath('/html/body/div[1]/div/main/div[1]/div/div/div/div/div/div/div/div/div[2]/div/div/div/ul/li/a')
+			more_athle.click()
+		except:
+			break
+	last_height =new_height
+html=driver.page_source 
+soup=BeautifulSoup(html,'lxml')   #pip install lxml.
+athlete_num = 1 #for counting athlete print 
+c_mem=10
+driver.get("https://www.google.co.kr/imghp?hl=ko&tab=wi&ogbl")
+for anchor in soup.select("div.c-listing-athlete-flipcard__text__back span.c-listing-athlete__name"): #bring soup data and download 50ish img
+	if athlete_num>c_mem:
+		driver.quit()
+		#subprocess.call("TASKKILL /f  /IM  C:/sel/selenium/chromedriver.exe")  
+		print( "%s memory resting" %(str(c_mem)) )
+		time.sleep(10)
+		driver = webdriver.Chrome()
+		driver.get("https://www.google.co.kr/imghp?hl=ko&tab=wi&ogbl")
+		c_mem+=10
+	print(  "%s: %s"  %(str(athlete_num) ,anchor.get_text()) )
+	athlete_num+=1
+	elem=driver.find_element_by_name("q")
+	driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.HOME) #scroll up 
+	elem.clear()
+	time.sleep(2)
+	elem.send_keys(anchor.get_text())
+	time.sleep(3)
+	athele_data=driver.find_element_by_css_selector(".JSAgYe").get_attribute("value") #athlete name
+	images=driver.find_elements_by_css_selector(".rg_i.Q4LuWd")
+	pic_count=1		#count download img
+	for image in images: 
+		try:
+			image.click()
+		except:
+			driver.execute_script("arguments[0].click();", image)
+		time.sleep(2)
+		imgUrl=driver.find_element_by_xpath('/html/body/div[2]/c-wiz/div[3]/div[2]/div[3]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div/div[2]/a/img').get_attribute("src")
+		cookies = dict(BCPermissionLevel='PERSONAL')
+		req = urllib.request.Request(imgUrl, headers={'User-Agent': 'mozilla'})
+		try:
+			res = urllib.request.urlopen(req)
+		except:
+			continue
+	
+		#assert res.status == 200
+		if res.status !=200:
+			print("it could have 200 error bro! delete this code before commit")
+			continue
+		
+		with open('%s%s.jpg' %(athele_data, str(pic_count)), 'wb') as out:
+				shutil.copyfileobj(res, out)
+
+		# with open( '%s%s.jpg' %(athele_data, str(pic_count)), 'wb') as handle:
+		# 	response = requests.get(imgUrl, headers={"User-Agent": "Mozilla/5.0"}, cookies=cookies,stream=True)
+		# 	if not response.ok:
+		# 		print (response)
+		# 	for block in response.iter_content(1024):
+		# 		if not block:
+		# 			break
+		# 	handle.write(block)
+		pic_count+=1
+			
+
+print("misson complete!!")
+driver.quit()
+	#subprocess.call("TASKKILL /f  /IM  CHROMEDRIVER.EXE") 
+
+
+
